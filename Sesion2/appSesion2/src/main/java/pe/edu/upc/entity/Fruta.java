02c82017166c9371a1caa59f01f7b557136a4a2d
unknown <Opeador@A702-20.upc.edu.pe>
2016-08-18 10:19:53 -0500

    Clases y Objetos

---
+++ Sesion2/appSesion2/src/main/java/pe/edu/upc/entity/Fruta.java
@@ -0,0 +1,54 @@
+package pe.edu.upc.entity;
+
+public class Fruta {
+	
+	// atributos de instancia
+	private String color;
+	private String sabor;
+	
+	//atributos de clase
+	public static String LugarOrigen="UPC";
+		
+	//Constructores
+	
+	public Fruta(String color, String sabor) {
+		super();
+		this.color = color;
+		this.sabor = sabor;
+	}
+
+	// Metodos 
+	
+	public static void SoyunmetodoGlobal()
+	{
+		
+	}
+	
+	public void SoyunMetodoObjeto()
+	{
+		
+	}
+	
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getSabor() {
+		return sabor;
+	}
+
+	public void setSabor(String sabor) {
+		this.sabor = sabor;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString()+"-"+this.getColor();
+	}
+
+}
